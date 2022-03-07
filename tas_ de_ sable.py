308013cb9440025729750bfd84644617143519f6
Nathan VILLIERS <nathan.villiers@ens.uvsq.fr>
2022-03-07 23:48:55 +0100

    config aléatoire

--- tas_ de_ sable.py
+++ tas_ de_ sable.py
@@ -19,7 +19,11 @@ m = int(input("coté de la grille"))
 
 largeur=500
 hauteur=500
-
+taillecarré=500/m
+x0=1
+y0=1
+L=[]
+configcourante=L  
 
 #DEFINITION DES VARIABLES GLOBALES
 
@@ -32,6 +36,39 @@ hauteur=500
 def test():
     print("le bouton marche")
 
+def basique ():
+    L=[]
+    for i in range (m):
+        L.append([])
+
+        for k in range (m):
+
+            try:
+                L[i].append(0)
+
+            except IndexError:
+                continue
+    print(L)
+
+def aléatoire ():
+    L=[]
+    for i in range (m):
+            L.append([])
+
+            for k in range (m):
+
+                try:
+
+                    L[i].append(r.randint(0,4))
+                
+
+                except IndexError:
+                    continue
+    print(L)
+
+def couleurs ():
+    
+
 #PROGRAMME
 
 #---création de l'interface graphique
@@ -42,15 +79,24 @@ root.geometry("1920x1080")
 canvas = tk.Canvas(root,width=largeur,height=hauteur,bg="#000000")
 canvas.grid(row=3,column=3,rowspan=1,columnspan=1)
 
-bouton_config_alea = tk.Button(root, text="Clique pour Configuration Aléatoire",command=test)
+bouton_config_alea = tk.Button(root, text="Clique pour Configuration Aléatoire",command=aléatoire)
 bouton_config_alea.grid(row=0,column=0)
+bouton_config_basique = tk.Button(root, text="Clique pour Configuration basique",command=basique)
+bouton_config_basique.grid(row=1,column=0)
+bouton_lancement = tk.Button(root, text="Clique pour colorer les cases",command=couleurs) 
+bouton_lancement.grid(row=2,column=0)
 
 #---création de la grille
 for i in range (m):
-    canvas.create_line(((largeur/m)*i,0), ((largeur/m)*i, 500), fill="blue", width=2)
-    canvas.create_line((0,(hauteur/m)*i), (500,(hauteur/m)*i), fill="blue", width=2)
+    canvas.create_line(((largeur/m)*i,0), ((largeur/m)*i, 500), fill="#474747", width=1)
+    canvas.create_line((0,(hauteur/m)*i), (500,(hauteur/m)*i), fill="#474747", width=1)
 
-L = [[0] * m] * m
+L = []
+
+
+
+    
 print(L)
 
 root.mainloop()
+
